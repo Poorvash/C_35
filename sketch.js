f65f9aa4c2d3818c8164a8c307ffced5979f4541
Poorvash <60345812+Poorvash@users.noreply.github.com>
2020-03-19 16:59:55 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+var  database;
+var gameState=0,playerCount;
+var player,game,form;
+
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+    game= new Game();
+    game.getState();
+    game.start();
+  
+}
+
+function draw(){
+  background("black");
+  
+   
+
+    drawSprites();
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
